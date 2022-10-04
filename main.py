f39b8043abd835e95cf907bad25a6c4adf691fa6
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2022-10-04 17:25:32 +0000

    FOR LOOPS AND WHILE LOOPS

---
+++ main.py
@@ -0,0 +1,35 @@
+# # fork the repl.it and create a new git hub repo entitled
+# #Comparison operators
+# # Logical operators
+# # Decision making
+# # loops(for loops, while loops, range, enumerator)
+# # min/max practice
+# # Random in python
+# # List comprehension
+# #importing modules
+# # modualarizing your code
+# #work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# # from dictionaries_loops import dictionaries_loops
+from loops import loop
+# from enumerators import enumerator
+# from list_comprehension import list_comprehensions
+# from logical_operators import logical_operator
+# from min_max import min_maxs
+# from random import randoms
+# from ranges import range
+# from while_loops import while_loop
+# from zip import zips
+# decisions()
+# comparison()
+loop()
+# enumerator()
+# list_comprehensions()
+# logical_operator()
+# min_maxs()
+# randoms()
+# range()
+# while_loop()
+# zips() 
